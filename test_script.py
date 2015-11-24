1a66fd09f8cd8d8d44b57452ac2f1d7dc26fb872
pavche <pavlin@varna.net>
2015-11-24 12:56:38 +0100

    Script commit from PC Elanius

--- test_script.py
+++ test_script.py
@@ -3,4 +3,4 @@
 print('This is a test script in Python')
 print('It will be put into GitHub repository in a new branch\nthus testing source control with GitHub')
 print('After the changes were made in this file, GitHub notices the modification.')
-
+print('Addind a new line from host Elanius.')
