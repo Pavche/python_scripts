6bde8ed7496d515093a654b6046258cbbb0a2c4b
Pavling Georgiev <georgiev@ankh.phil.muni.cz>
2015-11-24 12:05:30 +0100

    Modifying the contents of the new branch.

--- test_script.py
+++ test_script.py
@@ -2,3 +2,5 @@
 
 print('This is a test script in Python')
 print('It will be put into GitHub repository in a new branch\nthus testing source control with GitHub')
+print('After the changes were made in this file, GitHub notices the modification.')
+
