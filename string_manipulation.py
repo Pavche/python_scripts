6dd4f92b92882e6020e4bb76de8ba618440c5266
pavche <pavlin@varna.net>
2015-11-24 14:28:00 +0100

    New script for string manipulation

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # This script demonstrates string manipulation
 # The upper(), lower(), isupper(), and islower() String Methods
+# The startswith() and endswith() String Methods
+# The join() and split() String Methods
 
 raw_str = r'These are a raw strings. That is Carol\'s cat. That is Kate\'s dog'
 print(raw_str)
@@ -9,7 +11,6 @@ str1 = 'Hello world!'
 str1_up = str1.upper()
 str1_low= str1.lower()
 print('Normal case:' + str1_low + '\nLower case:' + str1_low +'\nUpper case:' + str1_up)  
-
 
 print('Please, enter a string.')
 str_new = input()
@@ -25,3 +26,22 @@ if str_new.islower():
 	print('Yes.')
 else:
 	print('No.')
+
+print('Hello world!')
+print('Does \'Hello world!\' starts with \'Hello\'?')
+'Hello world!'.startswith('Hello')
+print('Does \'Hello world!\' ends with \'world!\'?')
+'Hello world!'.endswith('Hello')
+
+print('\nJoining strings')
+str1=' '.join(['My','name','is','Simon'])
+str2='ABC'.join(['My', 'name', 'is', 'Simon'])
+
+# Rainbow colors
+str3=', '.join(['The rainbow colors are:','red','orange','yellow','green','blue','indigo','violet'])
+print(str1,'\n',str2,'\n',str3)
+
+print('\nAligned text')
+print('Hello (right-justify)'.rjust(30,'_'))
+print('Hello (right-justify)'.rjust(30,'>'))
+print('Hello (left-justify)'.ljust(30, '<'))
