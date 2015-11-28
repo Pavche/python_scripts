ef242fefab42576931de4cbe35e8d6f3f0b6e527
Pavlin Georgiev <georgiev@localhost.localdomain>
2015-11-28 12:45:32 +0100

    Modified working algorithm

--- google_map_autoviewer.py
+++ google_map_autoviewer.py
@@ -13,13 +13,13 @@ import subprocess
 
 Xmax,Ymax=pyautogui.size( )
 # Open a web browser
-browser_linux='/usr/bin/firefox' #For Linux Fedora
+browser_linux='/usr/bin/google-chrome' #For Linux Fedora
 browser_macosx='/Applications/Firefox.app/Contents/MacOS/firefox' #For MAC OS X 10.6 or above
 browser_win32='C:\\Program Files\\Mozilla Firefox\\firefox.exe'
 browser_win64='C:\Program Files (x86)\Mozilla Firefox\\firefox.exe'
-location_on_map='https://www.google.cz/maps/@43.2048071,27.9108268,3a,75y,357.62h,90.25t/data=!3m6!1e1!3m4!1sOCU_Az6N3o3XYteUv-VkMQ!2e0!7i13312!8i6656!6m1!1e1?hl=cs'
+location_on_map='https://www.google.cz/maps/@43.2266496,27.9258776,3a,75y,306.16h,74.88t/data=!3m6!1e1!3m4!1sJqD4dCizlZZln3Y0dh26-Q!2e0!7i13312!8i6656?hl=cs'
 
-subprocess.Popen([browser1,location_on_map])
+subprocess.Popen([browser_linux,location_on_map])
 
 # Making loop for several mouse clicks
 while True:
