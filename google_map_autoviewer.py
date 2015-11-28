7c652ce1dc12b715b972d7940a64d1349b2385a0
Pavlin Georgiev <pavlin@varna.net>
2015-11-28 11:43:05 +0100

    New functions to google autoviewer

--- google_map_autoviewer.py
+++ google_map_autoviewer.py
@@ -9,13 +9,17 @@
 import pyautogui
 import time
 import subprocess
+import quartz
 
 Xmax,Ymax=pyautogui.size( )
 # Open a web browser
-browser='/usr/bin/firefox'
+browser_linux='/usr/bin/firefox' #For Linux Fedora
+browser_macosx='/Applications/Firefox.app/Contents/MacOS/firefox' #For MAC OS X 10.6 or above
+browser_win32='C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+browser_win64='C:\Program Files (x86)\Mozilla Firefox\\firefox.exe'
 location_on_map='https://www.google.cz/maps/@43.2048071,27.9108268,3a,75y,357.62h,90.25t/data=!3m6!1e1!3m4!1sOCU_Az6N3o3XYteUv-VkMQ!2e0!7i13312!8i6656!6m1!1e1?hl=cs'
 
-subprocess.Popen([browser,location_on_map])
+subprocess.Popen([browser1,location_on_map])
 
 
 # Making loop for several mouse clicks
