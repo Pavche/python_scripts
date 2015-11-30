b24d70b0458f7e9a78fec307190d44b6fbbb132b
pavche <pavlin@varna.net>
2015-11-30 16:13:08 +0100

    Five bulgarian cities where included into Google map autoviewer

--- google_map_autoviewer.py
+++ google_map_autoviewer.py
@@ -14,6 +14,7 @@ import time
 import platform # Info about computer platform uder which the script is run
 import os
 import subprocess
+import sys
 # import quartz #For GUI under OS X on iMAC
 
 # List of web browsers under various OS
@@ -36,15 +37,18 @@ BG_Sofia_map='https://www.google.cz/maps/@42.6977942,23.3210398,3a,75y,102.62h,7
 BG_Plovdiv_map='https://www.google.cz/maps/@42.1472198,24.7481798,3a,75y,344.19h,78.09t/data=!3m6!1e1!3m4!1sGfD9ssq_CynA43s0XLUZyQ!2e0!7i13312!8i6656!6m1!1e1?hl=cs'
 BG_Stara_Zagora_map='https://www.google.cz/maps/@42.4240528,25.6255566,3a,75y,88.45h,79.35t/data=!3m6!1e1!3m4!1shvxQ66HS9HtwVTWSrR5g_Q!2e0!7i13312!8i6656!6m1!1e1?hl=cs'
 BG_Burgas_map='https://www.google.cz/maps/@42.4936817,27.4723975,3a,75y,96.07h,75.63t/data=!3m6!1e1!3m4!1sA4bHAW3SsD5WL7R0BwGfTw!2e0!7i13312!8i6656?hl=cs'
-BG_Varna_map='https://www.google.cz/maps/@43.2045882,27.9108968,3a,75y,342.42h,81.33t/data=!3m6!1e1!3m4!1smAUv2xQ8pOOlZ_f0Nh0jwQ!2e0!7i13312!8i6656!6m1!1e1?hl=cs'
+BG_Varna_map='https://www.google.cz/maps/@43.2046937,27.9108173,3a,75y,343.6h,80.57t/data=!3m6!1e1!3m4!1svtMhmXJ-b_ITuagCoK03zw!2e0!7i13312!8i6656?hl=cs'
 
 # Choose your destination on Google maps
 choice=0
 while choice not in range(1,6):
       print('Cities in Bulgaria\n1. Sofia\n2. Plovdiv\n3. Stara Zagora\n4. Burgas\n5. Varna')
-      print('ESC for quit')
+      print('Q for quit')
       try:
-      	  choice=int(raw_input('Your choice is: '))
+      	  choice=raw_input('Your choice is: ')
+	  if choice=='q' or choice=='Q':
+		sys.exit(0) 
+	  choice=int(choice) # Try to read as a number
       except ValueError:
           print "Not a number"
 
@@ -61,11 +65,12 @@ elif choice==5:
    location_on_map=BG_Varna_map
 else:
    print('Invalid choice. Exiting with error.')
+   sys.exit(1)
 
 subprocess.Popen([browser,location_on_map])
 
-# Wait for 5 sec till the web page is fully loaded
-time.sleep(7)
+# Wait for 10 sec till the web page is fully loaded
+time.sleep(10)
 
 # How to start browser in maximized window?
 # For Firefox press F11
